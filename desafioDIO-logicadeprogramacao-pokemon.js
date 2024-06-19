4cfb45cdbb64e6143261ad5657398c77a142800c
Alexia Camara <106205139+ginger-exe@users.noreply.github.com>
2024-06-19 10:40:29 -0300

    Update desafioDIO-logicadeprogramacao-pokemon.js

--- desafioDIO-logicadeprogramacao-pokemon.js
+++ desafioDIO-logicadeprogramacao-pokemon.js
@@ -1,5 +1,6 @@
-
-// Desafio DIO.me Felipão
+/*Crie um código para armazenar o máximo de informações possível dos pokemon da imagem ao lado e ao final, 
+para cada um, exiba uma mensagem de saída escrita o nome do pokemon concatenado com "Cadastrado com sucesso"
+Desafio DIO.me Felipão */
 
 let pokemons = []; // array para armazenar os pokemons
 
